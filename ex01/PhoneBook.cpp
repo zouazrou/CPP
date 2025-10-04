d540cb0d2b4a650c3bee5e8a006a000542ec0639
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-10-04 16:27:20 +0100

    next-step: sepate fucnton

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,24 +6,24 @@
 /*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 09:40:17 by zouazrou          #+#    #+#             */
-/*   Updated: 2025/10/04 12:34:08 by zouazrou         ###   ########.fr       */
+/*   Updated: 2025/10/04 16:26:09 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
 
-void padding(size_t size)
+void    padding(size_t size)
 {
     int padding_len;
     
-    padding_len = 10 - size;
+    padding_len = COL_WIDE - size;
     for (int i = 0; i < padding_len; i++)
         std::cout << ' ';
 }
 
 void    print_column(std::string str)
 {
-    if (str.length() <= 10)
+    if (str.length() <= COL_WIDE)
     {
         std::cout << str;
         padding(str.length());
@@ -35,40 +35,37 @@ void    print_column(std::string str)
 
 int trim_spaces(std::string &str)
 {
-    size_t i = 0;
+    int i;
+    int l;
     
-    while (i < str.length() && !isspace(str[i]))
+    i = 0;
+    l = str.length() - 1;
+    while (i < (int)str.length() && isspace(str[i]))
         i++;
-    str = str.erase(0, i);
+    while (l >= 0 && isspace(str[l]))
+        l--;
+    str.erase(l+1);
+    str.erase(0, i);
     if (str.empty())
         return (1);
     return (0);
 }
-void    PhoneBook::search(void)
+
+bool    isPhoneNum(std::string &s)
 {
-    // int i = index;
-    std::cout << "\n|  index   |";
-    std::cout << "first name|";
-    std::cout << "last name |";
-    std::cout << " Nickname |";
-    for (int i = 0; i < index; i++)
+    for (size_t i = 0; i < s.length(); i++)
     {
-        std::cout << "\n|__________|__________|__________|__________|\n";
-        std::cout << "|" << i << "         |";
-        print_column(contact[i].fName);
-        print_column(contact[i].lName);
-        print_column(contact[i].nickName);
-        i = (i+1) % MAX_CONTACT;
+        if (!isdigit(s[i]))
+            return (false);
     }
-    std::cout << "\n|__________|__________|__________|__________|\n";
+    return (true);
 }
+
 
 void    PhoneBook::add(void)
 {
-    if (index == MAX_CONTACT)
-        index = 0;
     /****/
-    std::cout << "-->First Name:" << std::endl;
+    std::cout << "-->first Name:" << std::endl;
     std::getline(std::cin, contact[index].fName);
     if (trim_spaces(contact[index].fName))
     {
@@ -78,17 +75,99 @@ void    PhoneBook::add(void)
     /****/
     std::cout << "-->Last Name:" << std::endl;
     std::getline(std::cin, contact[index].lName);
+    if (trim_spaces(contact[index].lName))
+    {
+        std::cerr << "IS EMPTY FIELD !\n";
+        return ;
+    }
     /****/
     std::cout << "-->NickName:" << std::endl;
     std::getline(std::cin, contact[index].nickName);
+    if (trim_spaces(contact[index].nickName))
+    {
+        std::cerr << "IS EMPTY FIELD !\n";
+        return ;
+    }
     /****/
     std::cout << "-->Phone Number:" << std::endl;
     std::getline(std::cin, contact[index].phoneNum);
+    if (trim_spaces(contact[index].phoneNum))
+    {
+        std::cerr << "IS EMPTY FIELD !\n";
+        return ;
+    }
+    if (isPhoneNum(contact[index].phoneNum) == false)
+    {
+        std::cerr << "PHONE-NUM MUST HAS ONLY DIGITS !\n";
+        return ;
+    }
     /****/
     std::cout << "-->Your Secret:" << std::endl;
     std::getline(std::cin, contact[index].secret);
-    /****/
-    index = (index + 1) % MAX_CONTACT;
+    if (trim_spaces(contact[index].secret))
+    {
+        std::cerr << "IS EMPTY FIELD !\n";
+        return ;
+    }
+    index = (index+1)%MAX_CONTACT;
+    
+    if (curr_size != MAX_CONTACT)
+        curr_size++;
+    std::cout << "next-idx->" << index << " :curr-size->" << curr_size << std::endl;
+}
+
+#include <unistd.h>
+
+// void    PhoneBook::display_contact_table(void)
+// {
+//     std::cout << "\n|  INDEX   |";
+//     std::cout << "first name|";
+//     std::cout << "last name |";
+//     std::cout << " Nickname |";
+//     int i = (index) % curr_size;
+//     int j;
+//     for (j = 0; j < curr_size; j++)
+//     {
+//         std::cout << "\n|__________|__________|__________|__________|\n";
+//         std::cout << "|" << j << "         |";
+//         print_column(contact[i].fName);
+//         print_column(contact[i].lName);
+//         print_column(contact[i].nickName);
+//         i = (i+1) % curr_size;
+//     }
+//     std::cout << "\n|__________|__________|__________|__________|\n";
+// }
+void    PhoneBook::search(void)
+{
+    
+    std::cout << "\n|  INDEX   |";
+    std::cout << "first name|";
+    std::cout << "last name |";
+    std::cout << " Nickname |";
+    int i = (index) % curr_size;
+    int j;
+    for (j = 0; j < curr_size; j++)
+    {
+        std::cout << "\n|__________|__________|__________|__________|\n";
+        std::cout << "|" << j << "         |";
+        print_column(contact[i].fName);
+        print_column(contact[i].lName);
+        print_column(contact[i].nickName);
+        i = (i+1) % curr_size;
+    }
+    std::cout << "\n|__________|__________|__________|__________|\n";
+    std::cout << "ENTRE THE CONTACT INDEX TO DISPLAY IT : ";
+    std::cin >> j;
+    if (!std::cin || j < 0 || j >= curr_size)
+    {
+        std::cerr << "Error : input is not valid\n";
+        return ;
+    }
+    std::cout << "First Name   : " << contact[(i+j)%curr_size].fName << std::endl;
+    std::cout << "Last Name   : " << contact[(i+j)%curr_size].lName << std::endl;
+    std::cout << "NickName     : " << contact[(i+j)%curr_size].nickName << std::endl;
+    std::cout << "Phone Number : " << contact[(i+j)%curr_size].phoneNum << std::endl;
+    std::cout << "Secret       : " << contact[(i+j)%curr_size].secret << std::endl;
 }
 
 void    PhoneBook::Exit(void)
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 15:01:02 by zouazrou          #+#    #+#             */
-/*   Updated: 2025/10/04 10:58:45 by zouazrou         ###   ########.fr       */
+/*   Updated: 2025/10/04 16:22:56 by zouazrou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,27 @@
 # include <cstdlib>
 # include "Contact.hpp"
 
-# define MAX_CONTACT 8
+# define MAX_CONTACT 3
+# define COL_WIDE 10
 
 class PhoneBook : public Contact
 {
     public:
         Contact contact[MAX_CONTACT];
         int  index;
-        bool    isFull;
-    PhoneBook()
-    {
-        index = 0;
-        isFull = false;
-    }    
-    void    add(void);
-    void    search(void);
-    void    Exit(void);
+        int  curr_size;
+        PhoneBook()
+        {
+            index = 0;
+            curr_size = 0;
+        }    
+        void    add(void);
+        void    search(void);
+        void    Exit(void);
+        
+    private:
+        void    display_contact_table(void);
+
 };
 
 #endif
