3390dec3b3a4a6aaf4661faf96e783b3163903de
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-10-04 12:35:15 +0100

    Error  add()

---
+++ ex01/test.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <iostream>
+
+int main() {
+    std::string s = "   hello world";
+
+    s.erase(0, s.find_first_not_of(' '));
+
+    std::cout << "[" << s << "]\n"; // prints "[hello world]"
+}
