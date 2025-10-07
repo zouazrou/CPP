fa1578c4695bc73db114da7ae0b477c73008d1a3
Zakaria Ouazrou <zouazrou@c1r9p3.1337.ma>
2025-10-07 17:16:02 +0100

    Create ex03 & Add header42 to ex2/main.cpp

---
+++ C01/ex03/Weapon.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zouazrou <zouazrou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/07 17:10:03 by zouazrou          #+#    #+#             */
+/*   Updated: 2025/10/07 17:13:05 by zouazrou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+#include <iostream>
+
+class Weapon
+{
+    private:
+        std::string type;
+    public:
+        Weapon();
+        ~Weapon();
+        std::string &getType(void);
+};
+
+Weapon::Weapon(/* args */)
+{
+}
+
+Weapon::~Weapon()
+{
+}
+
+#endif
