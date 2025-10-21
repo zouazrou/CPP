06f101bb8ecbf1c75b42f788885eeb9035d4f9bb
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-10-21 09:52:41 +0100

    cp ex00 to ex01

---
+++ C02/ex01/test.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    float nb = 1.4999;
+    cout << round(nb) << endl;     
+    return 0;
+}
