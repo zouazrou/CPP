1742ba4f18b6d25e7938488ef48f946f50555da7
Zakaria Ouazrou <zouazrou@c3r8p1.1337.ma>
2025-10-22 10:48:38 +0100

    Still Working on 00

--- C02/ex01/Fixed.cpp
+++ C02/ex01/Fixed.cpp
@@ -1,14 +1,23 @@
 #include "Fixed.hpp"
 
-static const int numFracBits = 8;
+const int Fixed::numFracBits = 8;
 
 Fixed::Fixed() : num(0)
 {
     std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(int num) : num(num)
-{}
+Fixed::Fixed(const int num)
+{
+    std::cout << "Int constructor called" << std::endl;
+    setRawBits(num * pow(2, numFracBits));
+}
+
+Fixed::Fixed(const float num)
+{
+    std::cout << "Float constructor called" << std::endl;
+    setRawBits(num * pow(2, numFracBits));
+}
 
 Fixed::Fixed(const Fixed& x)
 {
@@ -30,12 +39,22 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called" << std::endl;
+    // std::cout << "getRawBits member function called" << std::endl;
     return (num);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-    std::cout << "setRawBits member function called" << std::endl;
+    // std::cout << "setRawBits member function called" << std::endl;
     this->num = raw;
+}
+
+int Fixed::toInt( void ) const
+{
+    return (getRawBits() >> numFracBits);
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (getRawBits() / pow(2, numFracBits));
 }
