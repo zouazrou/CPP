cbc06628dd1ca5b7d5f4b1675b34832c5c7ab243
Zakaria Ouazrou <zouazrou@c3r1p3.1337.ma>
2025-10-25 22:00:24 +0100

    just Create Class

---
+++ C03/ex00/ClapTrap.cpp
@@ -0,0 +1,26 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+
+}
+
+ClapTrap::ClapTrap(std::string name) : name(name)
+{
+
+}
+
+ClapTrap::ClapTrap(const ClapTrap& c)
+{
+
+}
+
+ClapTrap& ClapTrap::operator =(const ClapTrap& c)
+{
+
+}
+
+ClapTrap::~ClapTrap()
+{
+
+}
---
+++ C03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap
+{
+private:
+    std::string name;
+    int         hitPoints;
+    int         energyPoints;
+    int         attackDamage;
+public:
+    // * Orthodox form
+    ClapTrap();
+    ClapTrap(std::string name);
+    ClapTrap(const ClapTrap& c);
+    ClapTrap& operator =(const ClapTrap& c);
+    ~ClapTrap();
+
+    // *
+    void    attack(const std::string& target);
+    void    takeDamage(unsigned int amount);
+    void    beRepaired(unsigned int amount);
+};
+
+ClapTrap::ClapTrap(/* args */)
+{
+}
+
+ClapTrap::~ClapTrap()
+{
+}
+
+
+#endif
