7279eb3ff9f0ae1ef81e90baa1a8e0140847268c
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-10-27 10:55:16 +0100

    Create ex02 and Add previous work

---
+++ C03/ex02/ClapTrap.cpp
@@ -0,0 +1,137 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap()
+{
+    this->name = "anonymous";
+    this->hitPoints = 10;
+    this->energyPoints = 10;
+    this->attackDamage = 0;
+    std::cout << "ClapTrap: Default Constructor Called" << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name)
+{
+    this->name = name;
+    // ! the robot's health
+    this->hitPoints = 10;
+    // ! It's the energy it needs to perform special actions
+    this->energyPoints = 10;
+    // ! the robot's offensive power
+    this->attackDamage = 0;
+    std::cout << "ClapTrap: Parameterized Constructor Called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& c)
+{
+    this->name = c.name;
+    this->hitPoints = c.hitPoints;
+    this->energyPoints = c.energyPoints;
+    this->attackDamage = c.attackDamage;
+    std::cout << "ClapTrap: Copy Constructor Called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator =(const ClapTrap& c)
+{
+    this->name = c.name;
+    this->hitPoints = c.hitPoints;
+    this->energyPoints = c.energyPoints;
+    this->attackDamage = c.attackDamage;
+    std::cout << "ClapTrap: Assignment operator Called" << std::endl;
+    return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap: Destructor Called" << std::endl;
+}
+
+void    ClapTrap::attack(const std::string& target)
+{
+    if (!this->hitPoints)
+    {
+        std::cout << "hit Points is Empty !" << std::endl;
+        return ;
+    }
+    if (!this->energyPoints)
+    {
+        std::cout << "Energy Points is Empty !" << std::endl;
+        return ;
+    }
+    std::cout << "ClapTrap " << this->name
+              << " attacks " << target
+              << ", causing " << this->attackDamage
+              << " points of damage!" << std::endl;
+    this->energyPoints--;
+}
+    
+void    ClapTrap::takeDamage(unsigned int amount)
+{
+    if ((int)amount > this->hitPoints)
+        amount = (unsigned int)this->hitPoints;
+    this->hitPoints -= amount;
+    std::cout << "ClapTrap " << this->name
+              << " loses " << amount
+              << " HitPoints!" << std::endl;
+}
+    
+void    ClapTrap::beRepaired(unsigned int amount)
+{
+    if (!this->hitPoints)
+    {
+        std::cout << "hit Points is Empty !" << std::endl;
+        return ;
+    }
+    if (!this->energyPoints)
+    {
+        std::cout << "Energy Points is Empty !" << std::endl;
+        return ;
+    }
+    this->hitPoints += amount;
+    this->energyPoints--;
+    std::cout << "ClapTrap "
+              << this->name
+              << " has been repaired by "
+              << amount
+              << std::endl;
+}
+
+// ********encapsulation***********
+std::string ClapTrap::getName(void) const
+{
+    return (this->name);
+}
+
+int         ClapTrap::getHitPoints(void) const
+{
+    return (this->hitPoints);
+}
+
+int         ClapTrap::getEnergyPoints(void) const
+{
+    return (this->energyPoints);
+}
+
+int         ClapTrap::getAttackDamage(void) const
+{
+    return (this->attackDamage);
+}
+
+void        ClapTrap::setName(std::string name)
+{
+    this->name = name;
+}
+
+void        ClapTrap::setHitPoints(int value)
+{
+    this->hitPoints = value;
+}
+
+void        ClapTrap::setEnergyPoints(int value)
+{
+    this->energyPoints = value;
+}
+
+void        ClapTrap::setAttackDamage(int value)
+{
+    this->attackDamage = value;
+}
---
+++ C03/ex02/ScavTrap.hpp
@@ -0,0 +1,19 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include "ClapTrap.hpp"
+#include <iostream>
+
+class ScavTrap : public ClapTrap
+{
+public:
+    ScavTrap();
+    ScavTrap(std::string name);
+    ScavTrap(const ScavTrap& c);
+    ScavTrap& operator =(const ScavTrap& src);
+    ~ScavTrap();
+
+    void guardGate(void);
+};
+
+#endif
