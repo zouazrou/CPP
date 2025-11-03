980944928c73fd3605d0c0fcd591e421e6e3c8b2
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-11-03 13:16:56 +0100

    character class not done

--- C04/ex03/Character.hpp
+++ C04/ex03/Character.hpp
@@ -1,12 +1,27 @@
-#include "AMateria.hpp"
-#include <iostream>
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
 
-class ICharacter
+#include <iostream>
+#include "AMateria.hpp"
+#include "ICharacter.hpp"
+
+class Character
 {
+// ! acces !
+    std::string name;
+    int         currSize;
+    AMateria    *slot[4];
 public:
-    virtual ~ICharacter() {}
-    virtual std::string const &getName() const = 0;
-    virtual void equip(AMateria *m) = 0;
-    virtual void unequip(int idx) = 0;
-    virtual void use(int idx, ICharacter &target) = 0;
+    Character();
+    Character(const Character& src);
+    Character& operator =(const Character& src);
+    // * pure member functions
+    ~Character();
+
+    std::string const   &getName() const;
+    void                equip(AMateria *m);
+    void                unequip(int idx);
+    void                use(int idx, Character &target);
 };
+
+#endif
