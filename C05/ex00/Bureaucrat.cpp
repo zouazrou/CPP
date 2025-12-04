7dd6d39ac09bfd39b8ae419bec1edbe2d272fb9c
zouazrou <you@example.com>
2025-12-04 15:31:32 +0100

    EX00 Done & with tests

--- C05/ex00/Bureaucrat.cpp
+++ C05/ex00/Bureaucrat.cpp
@@ -1,25 +1,55 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() 
-{
+Bureaucrat::Bureaucrat() : name(""), grade(150)
+{}
 
+Bureaucrat::Bureaucrat(std::string name, int grade): name(name)
+{
+    if (grade > 150)
+        throw GradeTooLowException();
+    else if (grade < 1)
+        throw GradeTooHighException();
+    else
+        this->grade = grade;
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& src)
 {
-
+    this->grade = src.grade;
 }
 
 Bureaucrat& Bureaucrat::operator =(const Bureaucrat& src)
 {
-
+    if (this != &src)
+        this->grade = src.grade;
+    return (*this);
 }
 
 Bureaucrat::~Bureaucrat()
-{
+{}
 
-}
 // *******
+std::ostream& operator<<(std::ostream &out, const Bureaucrat &c)
+{
+    out << c.getName() << ", bureaucrat grade " << c.getGrade() << ".";
+    return (out);
+}
+
+void    Bureaucrat::promote()
+{
+    if (this->grade == 1)
+        throw GradeTooHighException();
+    else
+        this->grade -= 1;
+}
+
+void    Bureaucrat::demote()
+{
+    if (this->grade == 150)
+        throw GradeTooLowException();
+    else
+        this->grade += 1;
+}
 
 std::string Bureaucrat::getName(void) const
 {
