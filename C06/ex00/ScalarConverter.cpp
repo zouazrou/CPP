c86b6ac59249f831e7c619f412201b4d214146f7
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-12-10 11:48:22 +0100

    Still working on cast funcions

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -5,10 +5,12 @@ ScalarConverter::ScalarConverter()
 
 ScalarConverter::ScalarConverter(const ScalarConverter& s)
 {
+    (void)s;
 }
 
 ScalarConverter& ScalarConverter::operator =(const ScalarConverter& s)
 {
+    (void)s;
     return (*this);
 }
 
@@ -18,4 +20,14 @@ ScalarConverter::~ScalarConverter()
 void    ScalarConverter::convert(std::string str)
 {
     t_types tp = getType(str);
+
+    if (tp == ERROR)
+    {
+        std::cout << "Error" << std::endl;
+        return ;
+    }
+    castToChar(str);
+    castToInt(str);
+    castToFloat(str);
+    castToDouble(str);
 }
--- C06/ex00/main.cpp
+++ C06/ex00/main.cpp
@@ -3,21 +3,19 @@
 
 int main(int ac, char *av[])
 {
-    // float fd = .1;
-
-    // std::cout << fd << std::endl;
     if (ac != 2)
         return (1);
-    else if (isChar(av[1]))
-        std::cout << "is char" << std::endl;
-    else if (isInt(av[1]))
-        std::cout << "is int" << std::endl;
-    else if (isFloat(av[1]))
-        std::cout << "is float" << std::endl;
-    else if (isDouble(av[1]))
-        std::cout << "is Double" << std::endl;
-    else
-        std::cout << "waaa3" << std::endl;
+    ScalarConverter::convert(av[1]);
+    // if (isChar(av[1]))
+    //     std::cout << "is char" << std::endl;
+    // else if (isInt(av[1]))
+    //     std::cout << "is int" << std::endl;
+    // else if (isFloat(av[1]))
+    //     std::cout << "is float" << std::endl;
+    // else if (isDouble(av[1]))
+    //     std::cout << "is Double" << std::endl;
+    // else
+    //     std::cout << "waaa3" << std::endl;
     
     return 0;
 }
