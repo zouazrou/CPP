bfc11865adab05449c38765c35ab78fd7c53f86d
Zakaria Ouazrou <zouazrou@c2r1p12.1337.ma>
2025-12-11 17:34:44 +0100

    ex01 done with his tests

---
+++ C06/ex01/Serializer.hpp
@@ -0,0 +1,22 @@
+#ifndef SERIALIZER_HPP
+# define SERIALIZER_HPP
+
+#include <iostream>
+#include <stdint.h>
+#include "Data.hpp"
+
+class Serializer
+{
+private:
+    Serializer();
+    Serializer(const Serializer &other);
+    Serializer &operator=(const Serializer &other);
+    ~Serializer();
+
+public:
+
+    static uintptr_t serialize(Data* ptr);
+    static Data* deserialize(uintptr_t raw);
+};
+
+#endif
