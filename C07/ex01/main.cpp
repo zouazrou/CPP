5de730b84530c1b535e06eec1c411a54a094343d
Zakaria Ouazrou <zouazrou@c1r2p14.1337.ma>
2025-12-12 22:28:03 +0100

    Add some tests

--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -27,9 +27,10 @@ int main()
 {
     int tab[] = {0, 1, 2, 3, 4};
     Awesome tab2[5];
+    float const tab3[] = {0.1f, 1.1f, 2.1f, 3.1f, 4.1f};
 
     iter(tab, 5, print<const int>);
     iter(tab2, 5, print<Awesome>);
-
+    iter(tab3, 5, print<const float>);
     return 0;
 }
