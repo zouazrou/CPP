09df84ea9c1fd2d5afc5d4de6eaf09f5874015bd
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-12-12 09:19:49 +0100

    Add try-catch block in ex02

--- C06/ex02/utils.cpp
+++ C06/ex02/utils.cpp
@@ -12,40 +12,60 @@ Base    *generate(void)
     val = rand() % 3;
     if (val == 0)
     {
-        std::cout << "Generated A" << std::endl;
+        std::cout << "Generated A obj" << std::endl;
         return (new A);
     }
     if (val == 1)
     {
-        std::cout << "Generated B" << std::endl;
+        std::cout << "Generated B obj" << std::endl;
         return (new B);
     }
-    std::cout << "Generated C" << std::endl;
+    std::cout << "Generated C obj" << std::endl;
     return (new C);
 }
 
 void    identify(Base* p)
 {
     if (dynamic_cast<A*>(p))
-        std::cout << "type : A" << std::endl;
+        std::cout << "type : A class" << std::endl;
     else if (dynamic_cast<B*>(p))
-        std::cout << "type : B" << std::endl;
+        std::cout << "type : B class" << std::endl;
     else if (dynamic_cast<C*>(p))
-        std::cout << "type : C" << std::endl;
+        std::cout << "type : C class" << std::endl;
     else
         std::cout << "I don't know -_-" << std::endl;
 }
 
 void    identify(Base& p)
 {
-    if (dynamic_cast<A*>(&p))
-        std::cout << "type : A" << std::endl;
-    else if (dynamic_cast<B*>(&p))
-        std::cout << "type : B" << std::endl;
-    else if (dynamic_cast<C*>(&p))
-        std::cout << "type : C" << std::endl;
-    else
-        std::cout << "I don't know -_-" << std::endl;
+    bool isFound = false;
+
+    try
+    {
+        dynamic_cast<A&>(p);
+        std::cout << "type : A class" << std::endl;
+        isFound = true;
+    }
+    catch(const std::exception& e)
+    {}
+    try
+    {
+        dynamic_cast<B&>(p);
+        std::cout << "type : B class" << std::endl;
+        isFound = true;
+    }
+    catch(const std::exception& e)
+    {}
+    try
+    {
+        dynamic_cast<C&>(p);
+        std::cout << "type : C class" << std::endl;
+        isFound = true;
+    }
+    catch(const std::exception& e)
+    {}
+    if (!isFound)
+        std::cout << "** Dont change in my Code -_-" << std::endl;
 }
 
 // ! RTTI (Run-Time Type Information) in C++
