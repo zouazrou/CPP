df5202eb633d4020a1007ffa773bbe9c5ded91c4
zouazrou <183734171+zouazrou@users.noreply.github.com>
2025-12-24 11:09:41 +0100

    Implement first step 'the division into the pairs & sorting'

--- C09/ex02/main.cpp
+++ C09/ex02/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+void printVector(const std::vector<int>& vec)
+{
+    std::cout << "[";
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << ", ";
+    }
+    std::cout << "]" << std::endl;
+}
 
 void    algo(std::vector<int>& input, int PairSize, int lvl)
 {
     int NumPairs;
+    // int tmp;
+    std::vector<int> a; // * big
+    std::vector<int> b; // * small
 
     // std::cout << "---------------------" << std::endl;
     lvl++;
@@ -12,27 +26,45 @@ void    algo(std::vector<int>& input, int PairSize, int lvl)
     else
         PairSize *= 2;
     NumPairs = input.size()/PairSize;
-    std::cout << "-->Lvl : " << lvl << " >> Number of Pairs :" << NumPairs << " >> size Pairs : " << PairSize << std::endl;
-    if (NumPairs > 0)
-        algo(input, PairSize, lvl);
-    std::cout << "-->Lvl : " << lvl << " >> Number of Pairs :" << NumPairs << " >> size Pairs : " << PairSize << std::endl;
-
-    
-    for (int it_num = 0; it_num < NumPairs; it_num++)
+    std::cout << "\033[1;31m-->Lvl : " << lvl << " >> Number of Pairs :" << NumPairs << " >> size Pairs : " << PairSize << "\033[0m" << std::endl;
+    if (NumPairs == 0)
     {
-        
+        std::cout << "Break recursive" << std::endl;
+        return ;
     }
-    
 
+    int SecondCompared;
+    int FirstCompared;
+    for (int i = 0; i < NumPairs; i++)
+    {
+        SecondCompared = PairSize * (i+1) - 1;
+        FirstCompared = SecondCompared - PairSize / 2;
+        a.push_back(SecondCompared);
+        b.push_back(FirstCompared);
+    }
+    std::cout << "** Tab Before Sort Pairs **" <<std::endl;    
+    printVector(input);
+    for (int i = 0; i < NumPairs; i++)
+    {
+        std::cout << "\033[1;32mcmp(" << input[b[i]] << " ? " << input[a[i]] << ")\033[0m" << std::endl;
+        if (input[b[i]] > input[a[i]])
+        {
+            std::cout << "\033[1;33mDo Swap !!\033[0m" <<std::endl;
+            // std::swap_ranges(input.begin()+b[i]+1, input.begin()+a[i]+1, input.begin()+b[i]-PairSize/2+1);
+            std::swap_ranges(input.begin()+a[i]-PairSize/2+1, input.begin()+a[i]+1, input.begin()+b[i]-PairSize/2+1);
+        }
+    }
+    std::cout << "** Tab after Sort Pairs **" <<std::endl;
+    printVector(input);
+    algo(input, PairSize, lvl);
 }
 
 int main()
-// int main(int ac, char *av[])
 {
     std::vector<int> input;
-    int arr[] = {5, 2, 8, 0, 1, 7, 6, 9, 4, 3};
+    int arr[] = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
     
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 22; i++)
         input.push_back(arr[i]);
 
 
